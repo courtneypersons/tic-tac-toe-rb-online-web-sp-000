1bee50a8c76a2987a686ff48f91a26865edd55c0
courtney persons <personscourtney@gmail.com>
2018-09-29 20:51:41 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -46,15 +46,13 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.chomp
-  if valid_move?(board, input)
-    move(board, input)
   index = input_to_index(input)
   if valid_move?(board, index)
     move(board, index)
     display_board(board)
   else
     turn(board)
-    end
+    
   end
 end
 
